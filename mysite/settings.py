ec7c79e50c035c07a8fe1ce446886598ec70daf5
mozillani <mozillani@hotmail.com>
2018-02-24 15:34:44 -0800

    adding allowed_host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9+tm7x+ejq6vt%!pjodxyf0djdh$(nghfi%*dqy(g#av35l0o+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywherecom']
+ALLOWED_HOSTS = ['127.0.0.1', 'mozillani.pythonanywhere.com', 'pythonanywhere.com']
 
 
 # Application definition
